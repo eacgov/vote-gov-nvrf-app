a21426e3c6c3adbffadcb3191488f79fd0599f84
Jacob Mangini <jacob.mangini@tactis.com>
2025-09-03 14:05:51 -0400

    EAC-458: Updated React App for Local Testing.

--- src/Utils/useFetchData.jsx
+++ src/Utils/useFetchData.jsx
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 
-const BASE_URL = "https://vote.gov";
+// const BASE_URL = "https://vote.gov";
+
+const BASE_URL = window.location.origin.includes("localhost:5173")
+  ? "https://vote-gov-acquia.ddev.site" // dev server should pull from DDEV
+  : window.location.origin;   // prod/stage uses current host
 
 export function useFetchData(url, uuid, first = false) {
   const { data, isError, isLoading } = useQuery({
     queryKey: [url + uuid],
     queryFn: async () => {
-      const response = await fetch(BASE_URL + url);
+      const response = await fetch(url);
       const fetchedData = await response.json();
       // If UUID is provided, filter data by UUID.
       const finalData = uuid
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,20 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     base: "./",
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://vote-gov-acquia.ddev.site",
+          changeOrigin: true,
+          secure: false,
+        },
+        "/nvrf": {
+          target: "https://vote-gov-acquia.ddev.site",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
     build: {
       rollupOptions: {
         input: {
